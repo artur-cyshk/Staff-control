2da4ad884b946388ba056447bc347d73eab5614b
Artur Vechorko <school26iko@gmail.com>
2015-12-21 17:15:17 +0300

    Update mongooseConnect.js
    change log

--- server/config/mongooseConnect.js
+++ server/config/mongooseConnect.js
@@ -1,6 +1,8 @@
 mongoose = require ('mongoose'),
 mongooseConnection = require ('./config.js');
 mongoose.connect (mongooseConnection.mongoose.uri,function  (err) {
-	console.log(err);
+	if(err){
+		console.log(err);	
+	}
 });
 module.exports = mongoose;
